7e523e3a6d54ba80c350f25263514bdbf4663d89
rostislavrotaru <rostislav@rotaru.me>
2017-08-05 22:49:58 -0700

    Initial check-in

---
+++ Roluan.Emulator.Common/Enums/TritState.cs
@@ -0,0 +1,12 @@
+﻿namespace Roluan.Emulator.Common.Enums
+{
+	/// <summary>
+	/// Represents the state of a <see cref="Trit"/>
+	/// </summary>
+	public enum TritState
+	{
+		Negative = -1,
+		Zero = 0,
+		Positive = 1
+	}
+}
