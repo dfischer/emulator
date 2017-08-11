c516aa789432cd9aa254e2daa4e0749911362ffd
rostislavrotaru <rostislav@rotaru.me>
2017-08-11 13:30:02 -0700

    Initial commit

---
+++ Roluan.Emulator.Common/Config.cs
@@ -0,0 +1,11 @@
+﻿namespace Roluan.Emulator.Common
+{
+	/// <summary>
+	/// Configuration class
+	/// </summary>
+	public static class Config
+	{
+		// Nine trits in one tryte
+		public const int TRYTE_SIZE = 9;
+	}
+}
---
+++ Roluan.Emulator.Common/Tryte.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Roluan.Emulator.Common
+{
+	/// <summary>
+	/// Class that represents a Tryte
+	/// </summary>
+	public class Tryte
+	{
+		#region Properties
+
+		public IList<Trit> Trits { get; private set; }
+
+		#endregion
+
+		public Tryte()
+		{
+			Initialize();
+		}
+
+		void Initialize()
+		{
+			Trits = new List<Trit>();
+
+			for (int i = 0; i < Config.TRYTE_SIZE; i++)
+			{
+				Trits.Add(new Trit());
+			}
+		}
+	}
+}
