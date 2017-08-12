466038f54d45fa370102c9d062f2033ae5a595aa
rostislavrotaru <rostislav@rotaru.me>
2017-08-12 10:52:53 -0700

    Initial commit

---
+++ Roluan.Emulator.Common/Gates/TNAND.cs
@@ -0,0 +1,17 @@
+﻿namespace Roluan.Emulator.Common.Gates
+{
+	public static class TNAND
+	{
+		/// <summary>
+		/// Represents The Ternary NOT AND gate. Represents the combination of <see cref="TNOT"/> and <see cref="TAND"/> 
+		/// gates.
+		/// </summary>
+		/// <param name="inputA">The first input <see cref="Trit"/></param>
+		/// <param name="inputB">The second input <see cref="Trit"/></param>
+		/// <returns>TNAND <see cref="Trit"/> output</returns>
+		public static Trit Output(Trit inputA, Trit inputB)
+		{
+			return TNOT.Output(TAND.Output(inputA, inputB));
+		}
+	}
+}
