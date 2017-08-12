4c3423953282080f400fdde750e04a69b07c0b5a
rostislavrotaru <rostislav@rotaru.me>
2017-08-12 10:49:26 -0700

    Initial commit

---
+++ Roluan.Emulator.Common/Gates/TAND.cs
@@ -0,0 +1,34 @@
+﻿using Roluan.Emulator.Common.Enums;
+
+namespace Roluan.Emulator.Common.Gates
+{
+	/// <summary>
+	/// Represents The Ternary AND gate
+	/// </summary>
+	public static class TAND
+	{
+		public static Trit Output(Trit inputA, Trit inputB)
+		{
+			Check.IsNull(inputA);
+			Check.IsNull(inputB);
+
+			if ((inputA.State == TritState.Negative && inputB.State == TritState.Zero) 
+	         || (inputA.State == TritState.Zero && inputB.State == TritState.Negative)
+	         || (inputA.State == TritState.Negative && inputB.State == TritState.Negative)
+		     || (inputA.State == TritState.Positive && inputB.State == TritState.Negative)
+		     || (inputA.State == TritState.Negative && inputB.State == TritState.Positive))
+			{
+				return new Trit(TritState.Negative);
+			}
+
+			if ((inputA.State == TritState.Positive && inputB.State == TritState.Zero)
+			 || (inputA.State == TritState.Zero && inputB.State == TritState.Positive))
+			{
+				return new Trit(TritState.Zero);
+			}
+
+
+			return new Trit(TritState.Zero);
+		}
+	}
+}
