5f1000dd5e316edc0b37ab6e2742bc81d9e6dfb2
rostislavrotaru <rostislav@rotaru.me>
2017-08-12 10:55:27 -0700

    Initial commit

---
+++ Roluan.Emulator.Common/Gates/TOR.cs
@@ -0,0 +1,37 @@
+﻿using Roluan.Emulator.Common.Enums;
+
+namespace Roluan.Emulator.Common.Gates
+{
+	public static class TOR
+	{
+		/// <summary>
+		/// Represents a Ternary OR Gate
+		/// </summary>
+		/// <param name="inputA">The first input <see cref="Trit"/></param>
+		/// <param name="inputB">The second input <see cref="Trit"/></param>
+		/// <returns>Ternary OR <see cref="Trit"/> output</returns>
+		public static Trit Output(Trit inputA, Trit inputB)
+		{
+			Check.IsNull(inputA);
+			Check.IsNull(inputB);
+
+			if (inputA.State == TritState.Negative && inputB.State == TritState.Negative)
+			{
+				return new Trit(TritState.Negative);
+			}
+
+			if ((inputA.State == TritState.Negative && inputB.State == TritState.Zero)
+			 || (inputA.State == TritState.Zero && inputB.State == TritState.Negative))
+			{
+				return new Trit(TritState.Zero);
+			}
+
+			if (inputA.State == TritState.Zero && inputB.State == TritState.Zero)
+			{
+				return new Trit(TritState.Zero);
+			}
+
+			return new Trit(TritState.Positive);
+		}
+	}
+}
