779e8df7be820542e518b45040797f3e09ed9e2e
rostislavrotaru <rostislav@rotaru.me>
2017-08-12 11:31:44 -0700

    Initial commit

---
+++ Roluan.Emulator.Common/Gates/TSUM.cs
@@ -0,0 +1,43 @@
+﻿using Roluan.Emulator.Common.Enums;
+
+namespace Roluan.Emulator.Common.Gates
+{
+	public static class TSUM
+	{
+		/// <summary>
+		/// Represents Ternary summator
+		/// </summary>
+		/// <param name="inputA">The first input <see cref="Trit"/></param>
+		/// <param name="inputB">The second input <see cref="Trit"/></param>
+		/// <returns>Ternary SUM <see cref="Trit"/> output</returns>
+		public static Trit Output(Trit inputA, Trit inputB)
+		{
+			Check.IsNull(inputA);
+			Check.IsNull(inputB);
+
+			if (inputA.State == TritState.Negative && inputB.State == TritState.Negative)
+			{
+				return new Trit(TritState.Positive);
+			}
+
+			if (inputA.State == TritState.Positive && inputB.State == TritState.Positive)
+			{
+				return new Trit(TritState.Negative);
+			}
+
+			if ((inputA.State == TritState.Negative && inputB.State == TritState.Zero)
+			 || (inputA.State == TritState.Zero && inputB.State == TritState.Negative))
+			{
+				return new Trit(TritState.Negative);
+			}
+
+			if ((inputA.State == TritState.Positive && inputB.State == TritState.Zero)
+			 || (inputA.State == TritState.Zero && inputB.State == TritState.Positive))
+			{
+				return new Trit(TritState.Positive);
+			}
+
+			return new Trit(TritState.Zero);
+		}
+	}
+}
