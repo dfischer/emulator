70dcd68f033ada4000375d24adfe213a02b358d7
rostislavrotaru <rostislav@rotaru.me>
2017-08-12 10:58:43 -0700

    Initial commit

---
+++ Roluan.Emulator.Common/Gates/TNXOR.cs
@@ -0,0 +1,16 @@
+﻿namespace Roluan.Emulator.Common.Gates
+{
+	public static class TNXOR
+	{
+		/// <summary>
+		/// Represents a Ternary NOT XOR gate.
+		/// </summary>
+		/// <param name="inputA">The first input <see cref="Trit"/></param>
+		/// <param name="inputB">The second input <see cref="Trit"/></param>
+		/// <returns>Ternary NOT XOR <see cref="Trit"/> output</returns>
+		public static Trit Output(Trit inputA, Trit inputB)
+		{
+			return TNOT.Output(TXOR.Output(inputA, inputB));
+		}
+	}
+}
